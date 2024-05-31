b76e4b032d18695fb54277b32f97914fdfa25c07
Aleksander <aleksievisa@gmail.com>
2024-05-31 18:50:43 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h3>{tg ? JSON.stringify(tg) : 'НЕТУ'}</h3>
+      <h3>{tg ? <p>{JSON.stringify(tg.initDataUnsafe)}</p> : 'НЕТУ'}</h3>
     </main>
   )
 }
