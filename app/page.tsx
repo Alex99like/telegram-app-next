496615961bc46c1dddaaf6918209ad145f64e722
Aleksander <aleksievisa@gmail.com>
2024-05-31 18:35:37 +0300

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,33 @@
 'use client'
 
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [tg, setTg] = useState<WebApp | null>(null)
+
+  useEffect(() => {
+    console.log('useTelegram')
+    function initTg() {
+      if (
+        typeof window !== 'undefined' &&
+        window.Telegram &&
+        window.Telegram.WebApp
+      ) {
+        console.log('Telegram WebApp is set')
+        const tgData = window.Telegram.WebApp
+        setTg(tgData)
+      } else {
+        console.log('Telegram WebApp is undefined, retrying…')
+        setTimeout(initTg, 500)
+      }
+    }
+    initTg()
+  }, [])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h3>
-        {window?.Telegram?.WebApp?.initDataUnsafe.user?.first_name || 'НЕТУ'}
-      </h3>
+      <h3>{tg?.initDataUnsafe.user?.first_name || 'НЕТУ'}</h3>
     </main>
   )
 }
